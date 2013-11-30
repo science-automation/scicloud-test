bb9009e4ca6af8c44d13831477e615ed58aaf134
root <root@scivm.(none)>
2013-11-30 22:50:37 +0200

    add template test case files for nose

---
+++ test.volume.ls/test.py
@@ -0,0 +1,36 @@
+#
+# Test cases for Science VM scicloud API
+#
+# Copyright 2014 Science Automation
+#
+import cloud
+from cloud import CloudException, CloudTimeoutError
+from nose import with_setup
+from nose.tools import *
+
+def setup_function():
+    pass
+ 
+def teardown_function():
+    pass
+
+def test_multiply():
+    jid = cloud.call(lambda: 3*3)
+    answer = cloud.result(jid)
+    assert answer == 9
+
+@raises(TypeError)
+def test_exception2():
+    '''Raise TypeError since cloud.call called without arguments'''
+    jid = cloud.call()
+
+@raises(TypeError)
+def test_exception3():
+    '''Raise TypeError since cloud.call called with 1 invalid argument'''
+    jid = cloud.call("asdf")
+
+@raises(TypeError)
+def test_exception4():
+    '''Raise TypeError since cloud.call called with 2 invalid arguments'''
+    jid = cloud.call("asdf","sadf")
+
--- test.volume/cloud.volume/get_list.py
+++
@@ -1,5 +0,0 @@
-import cloud
-
-if __name__ == '__main__':
-
-    cloud.volume.get_list()
