2d473e8a1075ee3980f557572287839c85507968
root <root@scivm.(none)>
2013-11-30 11:54:24 +0200

    improve volume tests

--- cloud.volume.delete/delete.py
+++ cloud.volume.delete/delete.py
@@ -1,5 +1,18 @@
 import cloud
 
+def setup_function():
+    # check if volume already exists and delete it
+    if not cloud.volume.get_list("mydata"):
+       cloud.volume.create("mydata", "/data", "This is a mount point")
+
+def teardown_function():
+    print "teardown completed"
+
+
 if __name__ == '__main__':
 
+    setup_function()
     cloud.volume.delete("mydata")
+    if not cloud.volume.get_list("mydata"):
+       print "mydata was deleted successfully"
+    teardown_function()
--- cloud.volume.get_list/get_list.py
+++ cloud.volume.get_list/get_list.py
@@ -1,5 +1,24 @@
 import cloud
 
+def setup_function():
+    # create some volumes that we can list
+    if not cloud.volume.get_list():
+        print "Setting up some volumes"
+        cloud.volume.create("mydata", "/data", "This is a mount point")
+        cloud.volume.create("mydata2", "/data2", "This is a mount point2")
+        cloud.volume.create("mydata3", "/data3", "This is a mount point3")
+
+def teardown_function():
+    list = cloud.volume.get_list()
+    for volume in list:
+       print "deleting volume"
+       cloud.volume.delete(volume['name'])
+
+
 if __name__ == '__main__':
 
-    cloud.volume.get_list()
+    setup_function()
+    if cloud.volume.get_list():
+       print "Found some volumes, print info about them"
+       print cloud.volume.get_list()
+    teardown_function()
