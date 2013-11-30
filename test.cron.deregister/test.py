1f7263d7cdceb0cd4cd97e1625c8d1ec37abd413
root <root@scivm.(none)>
2013-12-01 00:04:05 +0200

    add tests for cron

--- test.cron.deregister/test.py
+++ test.cron.deregister/test.py
@@ -8,29 +8,24 @@ from cloud import CloudException, CloudTimeoutError
 from nose import with_setup
 from nose.tools import *
 
+
 def setup_function():
-    pass
+    cloud.cron.register(lambda: 3*3, 'mycron', '5 0 * * *')
  
 def teardown_function():
     pass
 
-def test_multiply():
-    jid = cloud.call(lambda: 3*3)
-    answer = cloud.result(jid)
-    assert answer == 9
+@with_setup(setup_function, teardown_function)
+def test_deregister():
+    cloud.cron.deregister("mycron")
 
 @raises(TypeError)
 def test_exception2():
-    '''Raise TypeError since cloud.call called without arguments'''
-    jid = cloud.call()
+    '''Raise TypeError since cloud.deregister called without arguments'''
+    cloud.cron.deregister()
 
 @raises(TypeError)
 def test_exception3():
-    '''Raise TypeError since cloud.call called with 1 invalid argument'''
-    jid = cloud.call("asdf")
-
-@raises(TypeError)
-def test_exception4():
-    '''Raise TypeError since cloud.call called with 2 invalid arguments'''
+    '''Raise TypeError since cloud.deregister called with 2 invalid arguments'''
     jid = cloud.call("asdf","sadf")
 
--- test.cron.manual_run/test.py
+++ test.cron.manual_run/test.py
@@ -8,29 +8,23 @@ from cloud import CloudException, CloudTimeoutError
 from nose import with_setup
 from nose.tools import *
 
+
 def setup_function():
-    pass
+    cloud.cron.register(lambda: 3*3, 'mycron', '5 0 * * *')
  
 def teardown_function():
-    pass
+    cloud.cron.deregister("mycron")
 
-def test_multiply():
-    jid = cloud.call(lambda: 3*3)
-    answer = cloud.result(jid)
-    assert answer == 9
+@with_setup(setup_function, teardown_function)
+def test_manual():
+    cloud.cron.manual_run("mycron")
+
+@raises(TypeError)
+def test_exception1():
+    '''Raise TypeError since cloud.manual_run called without arguments'''
+    cloud.cron.manual_run()
 
 @raises(TypeError)
 def test_exception2():
-    '''Raise TypeError since cloud.call called without arguments'''
-    jid = cloud.call()
-
-@raises(TypeError)
-def test_exception3():
-    '''Raise TypeError since cloud.call called with 1 invalid argument'''
-    jid = cloud.call("asdf")
-
-@raises(TypeError)
-def test_exception4():
-    '''Raise TypeError since cloud.call called with 2 invalid arguments'''
-    jid = cloud.call("asdf","sadf")
-
+    '''Raise TypeError since cloud.manual_run called with 2 arguments'''
+    cloud.cron.manual_run("asdf","sadf")
