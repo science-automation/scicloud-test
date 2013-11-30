d185e72039e9281ccae3577d36477630483ed104
root <root@scivm.(none)>
2013-11-30 11:58:15 +0200

    update

--- cloud.volume.ls/ls.py
+++ cloud.volume.ls/ls.py
@@ -1,6 +1,19 @@
 import cloud
 
+def setup_function():
+    # check if volume already exists and delete it
+    if cloud.volume.get_list("mydata"):
+       cloud.volume.delete("mydata")
+
+def teardown_function():
+    if cloud.volume.get_list("mydata"):
+       cloud.volume.delete("mydata")
+
+
 if __name__ == '__main__':
 
+    setup_function()
     cloud.volume.create("mydata", "/data", "This is a mount point")
-    cloud.volume.sync("data","mydata")
+    files = cloud.volume.ls("mydata")
+    print files
+    teardown_function()
