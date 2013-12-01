bf49e8b61d6417a04f4fa4374ddff6a371e978b7
root <root@scivm.(none)>
2013-12-01 20:57:42 +0200

    initial commit of env test cases

---
+++ test.environment.create/test.py
@@ -0,0 +1,27 @@
+#
+# Test cases for Science VM scicloud API
+#
+# Copyright 2014 Science Automation
+#
+import cloud
+from cloud import CloudException, CloudTimeoutError
+from nose import with_setup
+from nose.tools import *
+
+def setup_function():
+#    cloud.environment.create('testenv','precise')
+    pass
+
+def teardown_function():
+    # unable to delete environments through api
+    pass
+
+@with_setup(setup_function, teardown_function)
+def test_environment_list():
+    list = cloud.environment.list_envs()
+    assert len(list) > 0
+
+@raises(TypeError)
+def test_exception1():
+    '''Raise TypeError since cloud.environment.list_envs called with 2 arguments'''
+    cloud.environment.list_envs('asdfd','asdfds')
---
+++ test.environment.modify/env_modify.py
@@ -0,0 +1,6 @@
+import cloud
+
+if __name__ == '__main__':
+
+    print cloud.environment.list_bases()
+    print cloud.environment.list_envs()
