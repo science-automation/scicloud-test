da168e80cfd3620ac7e9f4b5059673b41e7dabdb
root <root@scivm.(none)>
2013-12-01 16:49:05 +0200

    bucket sync tests

--- test.bucket.sync_to_cloud/test.py
+++ test.bucket.sync_to_cloud/test.py
@@ -9,28 +9,28 @@ from nose import with_setup
 from nose.tools import *
 
 def setup_function():
-    pass
+    cloud.bucket.put('test1.txt')
  
 def teardown_function():
-    pass
+    cloud.bucket.remove('test1.txt')
 
-def test_multiply():
-    jid = cloud.call(lambda: 3*3)
-    answer = cloud.result(jid)
-    assert answer == 9
+@with_setup(setup_function, teardown_function)
+def test_exists():
+    '''Sync File To Cloud'''
+    assert cloud.bucket.sync_to_cloud('test1.txt') == None
+
+@with_setup(setup_function, teardown_function)
+@raises(IOError)
+def test_no_exist():
+    '''File Does Not Exist'''
+    assert cloud.bucket.sync_to_cloud('doesnotexist.txt') == False
+
+@raises(TypeError)
+def test_exception1():
+    '''Raise TypeError since cloud.sync_to_cloud called without arguments'''
+    cloud.bucket.sync_to_cloud()
 
 @raises(TypeError)
 def test_exception2():
-    '''Raise TypeError since cloud.call called without arguments'''
-    jid = cloud.call()
-
-@raises(TypeError)
-def test_exception3():
-    '''Raise TypeError since cloud.call called with 1 invalid argument'''
-    jid = cloud.call("asdf")
-
-@raises(TypeError)
-def test_exception4():
-    '''Raise TypeError since cloud.call called with 2 invalid arguments'''
-    jid = cloud.call("asdf","sadf")
-
+    '''Raise TypeError since cloud.sync_to_cloud called with 4 arguments'''
+    cloud.bucket.sync_to_cloud('file1.txt','file2.txt','file3.txt','file4.txt')
